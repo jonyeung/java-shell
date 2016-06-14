bd99f8c5ca58c3f9be4fba037f28b5604e080579
chanja49 <chanja49@UTORONTO.CA>
2016-06-14 01:08:48 +0000

    fixed up history methods so that it works with textfile class

--- Assignment2/src/driver/History.java
+++ Assignment2/src/driver/History.java
@@ -2,13 +2,7 @@ package driver;
 
 public class History {
 
-  // TODO have another constructor in TextFile class so that
-  // it only needs the name of file
-
-  // TextFile class has 2 arguments while historyFile type TextFile takes in 1
-  // TODO please uncomment the line below and fix as it gives an error
-  // private static TextFile historyFile = new TextFile("history");
-
+  private static TextFile historyFile = new TextFile("history");
   private static int numLines = 0;
 
   /**
@@ -22,10 +16,7 @@ public class History {
 
     String appendLine = Integer.toString(numLines) + ". " + input + "\n";
     // add the input line to the history file
-    // TODO create an append method in TextFile class
-
-    // TODO uncomment the line below and fix
-    // historyFile.append(appendLine);
+    historyFile.append(appendLine);
   }
 
 
@@ -37,18 +28,13 @@ public class History {
    */
   public static void printHistory(int lastLines) {
     // separates the file contents by line into an array
-    // TODO add a read option to textfile class
-
-    // TODO uncomment this after fix
-    // String lines[] = Interpreter.inputToArray(historyFile.fileContents,
-    // "\n");
+    String lines[] = Interpreter.inputToArray(historyFile.getFileContents(),
+        "\n");
 
     int startLineIndex = numLines - lastLines;
     // loop through each line starting at startLineIndex and print them
     for (int i = startLineIndex; i < numLines; i++) {
-
-      // TODO uncomment this after fix
-      // System.out.println(lines[i]);
+      System.out.println(lines[i]);
     }
   }
 
