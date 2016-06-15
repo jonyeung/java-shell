1afd486ea9ef2871a5ce12ffa69a0ebdc5cae6f2
chanja49 <chanja49@UTORONTO.CA>
2016-06-15 18:02:52 +0000

    added DirectoryStack with pushd and popd methods. It still needs cd and pwd to work

---
+++ Assignment2/src/driver/DirectoryStack.java
@@ -0,0 +1,35 @@
+package driver;
+
+import java.util.Stack;
+
+public class DirectoryStack {
+
+  private static Stack<String> savedDirectories = new Stack<String>();
+
+  /**
+   * Puts the current working directory onto the stack of directories and 
+   * changes the current working directory to the one specified
+   * 
+   * @param changeDirectory The current directory will be changed to this
+   */
+  private static void pushd(String changeDirectory) {
+    // use pwd to get the name of the current directory
+
+    // push the current directory onto the stack
+    
+    // use cd to change into changeDirectory
+  }
+
+  /**
+   * Gets the first directory saved onto the stack and changed the current
+   * working directory to it
+   */
+  public static void popd() {
+    // pops the first element and use cd
+    // if there is no directory on the stack then raise exception
+    
+  }
+
+
+
+}
