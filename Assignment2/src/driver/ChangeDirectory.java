61bcd0b2a4d1cedf825d9bb9e081d903ebf7b829
yeungj21 <yeungj21@UTORONTO.CA>
2016-06-19 20:02:24 +0000

    Changed ChangeDirectory.java's method to be simplified

--- Assignment2/src/driver/ChangeDirectory.java
+++ Assignment2/src/driver/ChangeDirectory.java
@@ -5,45 +5,13 @@ public class ChangeDirectory {
   /**
    * Change the user's current directory
    * 
-   * @param curr The directory the user is currently in
+   * @param fileSystem The file system with all the files and directories
    * @param path The new directory the user changes to
    */
-  public static void changeCurrentDirectory(Directory curr, String path) {
-
-    // determine if the path is absolute or relative
-    // check if path is absolute or relative
-    boolean relative = true;
-
-    FileSystem fileSystem = new FileSystem();
-
-    if (path.charAt(0) == '/') {
-      relative = false;
-    }
-
-    if (relative) {
-
-      // find the child directory
-      File file = null;
-
-      for (int i = 0; i < curr.getStoredFiles().size(); i++) {
-
-        file = curr.getStoredFiles().get(i);
-
-        if (file.getName() == path) {
-          break;
-        }
-      }
-
-      // set the current directory to be the child file
-      fileSystem.setCurrentDirectory((Directory) file);
-
-    } else {
-
-      // get the file found at the end of the pathway
-      File end = fileSystem.traversePath(path);
-
-      // set the current directory to be this file
-      fileSystem.setCurrentDirectory((Directory) end);
-    }
+  public static void changeCurrentDirectory(FileSystem fileSystem, String path) {
+    // get the file from the path
+    File destinationFile = fileSystem.traversePath(path);
+    // set the current directory of fileSystem to this file
+    fileSystem.setCurrentDirectory((Directory) destinationFile);
   }
 }
