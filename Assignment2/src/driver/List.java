d966138ede968bd4b3db5891a9ad4cead59f1bea
yeungj21 <yeungj21@UTORONTO.CA>
2016-06-19 20:11:03 +0000

    Reformatted List.java to work by taking in a FileSystem

--- Assignment2/src/driver/List.java
+++ Assignment2/src/driver/List.java
@@ -5,11 +5,12 @@ public class List {
   /**
    * Prints the contents of the current directory.
    * 
-   * @param dir The current directory who's contents are to be printed
+   * @param fileSystem The file system containing all the files and directories
    */
-  public static void listContents(Directory dir) {
-
-    // Print all the elements in the storedFiles set in dir
+  public static void listContents(FileSystem fileSystem) {
+    // get the current directory the user is in
+    Directory dir = fileSystem.getCurrentDirectory();
+    // Print all the elements in the storedFiles found in dir
     for (File file : dir.getStoredFiles()) {
       System.out.println(file.getName());
     }
@@ -18,16 +19,17 @@ public class List {
   /**
    * Prints the contents of the directory found at the given pathway.
    * 
+   * @param fileSystem The file system containing all the files and directories
    * @param path The pathway specifying a certain file
    */
-  public static void printDirectoryContentsGivenPath(String path) {
-
-    // Initialize FileSystem object
-    FileSystem fileSystem = new FileSystem();
-
+  public static void printDirectoryContentsGivenPath(FileSystem fileSystem,
+      String path) {
     // reach the file found at the pathway
-    Directory file = (Directory) fileSystem.traversePath(path);
-    // list the contents of this file
-    List.listContents(file);
+    Directory dir = (Directory) fileSystem.traversePath(path);
+    // Print all the elements in the storedFiles found in dir
+    for (File file : dir.getStoredFiles()) {
+      System.out.println(file.getName());
+    }
   }
+
 }
