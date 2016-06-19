1f29d2a5ddacaa6de6f50c5812f6306d2fda3641
xiezhang <xiezhang@UTORONTO.CA>
2016-06-19 20:04:17 +0000

    updated change directory and made sure it doesnt exceed 80 character limit

--- Assignment2/src/driver/ChangeDirectory.java
+++ Assignment2/src/driver/ChangeDirectory.java
@@ -8,9 +8,12 @@ public class ChangeDirectory {
    * @param fileSystem The file system with all the files and directories
    * @param path The new directory the user changes to
    */
-  public static void changeCurrentDirectory(FileSystem fileSystem, String path) {
+  public static void changeCurrentDirectory(FileSystem fileSystem,
+      String path) {
+
     // get the file from the path
     File destinationFile = fileSystem.traversePath(path);
+ 
     // set the current directory of fileSystem to this file
     fileSystem.setCurrentDirectory((Directory) destinationFile);
   }
