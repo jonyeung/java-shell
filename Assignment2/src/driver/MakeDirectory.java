c9ffe7896601c016a33fe91f464cb6a2acad325a
yeungj21 <yeungj21@UTORONTO.CA>
2016-06-19 20:15:45 +0000

    Reformatted MakeDirectory.java to work with FileSystem

--- Assignment2/src/driver/MakeDirectory.java
+++ Assignment2/src/driver/MakeDirectory.java
@@ -6,32 +6,33 @@ public class MakeDirectory {
   /**
    * Create a directory given a relative file path
    * 
-   * @param curr The directory the user is currently in
+   * @param fileSystem The file system with all the files and directories
    * @param dirName The name of the new directory being made
    */
-  public static void mkdirRelative(Directory curr, String dirName) {
-    // create a new directory in curr
-    curr.storeFile(new Directory(dirName));
+  public static void mkdirRelative(FileSystem fileSystem, String dirName) {
+    // create a new directory in the current directory of the file system
+    fileSystem.getCurrentDirectory().storeFile(new Directory(dirName));
   }
-  
+
   /**
    * Create a directory given an absolute file path
    * 
+   * @param fileSystem The file system with all the files and directories
    * @param path The absolute file path of the new directory
    */
-  public static void mkdirAbsolute(String path) {
+  public static void mkdirAbsolute(FileSystem fileSystem, String path) {
     // split the path into a list
     String[] pathway = Interpreter.filepathToArray(path);
     // get the name of the new directory being made
     String newDir = pathway[-1];
-    
+
     // get the parent directory of the new directory
-    String newPath = path.substring(0, path.lastIndexOf("/"));
-    Directory parent = (Directory) FileSystem.traversePath(newPath);
-    
+    String parentPath = path.substring(0, path.lastIndexOf("/"));
+    Directory parent = (Directory) fileSystem.traversePath(parentPath);
+
     // create a new directory in the parent directory
     parent.storeFile(new Directory(newDir));
   }
-  
+
 
 }
