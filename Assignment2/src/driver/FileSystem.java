f38fdf889ea61fc3c8fc0fa7fd675f98e912a3c7
xiezhang <xiezhang@UTORONTO.CA>
2016-06-19 19:55:52 +0000

    updated FileSystem.java

--- Assignment2/src/driver/FileSystem.java
+++ Assignment2/src/driver/FileSystem.java
@@ -2,19 +2,12 @@ package driver;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-// TODO UNCOMMENT line below after traverse is complete
-// import java.util.Set;
-
-import driver.Node;
+import driver.File;
 
 /**
- * Represents a Tree of Object String. Starts as a single root which points to a
- * List<Node<String>> of children.
+ * Represents a Tree of object File. Starts as a single root
  */
 public class FileSystem {
-
-  private Node root;
 
   /**
    * Default Tree constructor
@@ -22,88 +15,6 @@ public class FileSystem {
   public FileSystem() {
 
     super();
-  }
-
-  /**
-   * Get root of tree
-   * 
-   * @return root
-   */
-  public Node getRoot() {
-
-    return this.root;
-  }
-
-  /**
-   * Set the root of tree
-   * 
-   */
-  public void setRoot(Node root) {
-
-    this.root = root;
-  }
-
-  /**
-   * Return List representation of Tree
-   * 
-   * @return treeList
-   */
-  public List<Node> treeToList() {
-
-    List<Node> treeList = new ArrayList<Node>();
-    this.traverse(root, treeList);
-    return treeList;
-  }
-
-  /**
-   * Return String representation of Tree
-   * 
-   * @return treeString
-   */
-  public String toString() {
-
-    String treeString = this.treeToList().toString();
-    return treeString;
-  }
-
-  /**
-   * Traverse tree pre-order.
-   * 
-   * @param startNode The starting node to begin traversal
-   * @param list The list of nodes
-   */
-  private void traverse(Node startNode, List<Node> list) {
-
-    list.add(startNode);
-    for (Node singleNode : startNode.getChildren()) {
-      this.traverse(startNode, list);
-    }
-
-    /**
-     * TODO UNCOMMENT AND COMPLETE Traverse the file system based on the given
-     * path.
-     * 
-     * @param path The pathway the file system is to traverse.
-     * @return file The file found at the end of the path.
-     */
-    // public File traversePath(String path) {
-    // // parse the path by converting it to a list
-    // String[] pathway = Interpreter.filepathToArray(path);
-    // // make sure root is in the path
-    // if (root.getName() != pathway[0]) {
-    // // TODO THROW AN EXCEPTION
-    // }
-    // else {
-    // // traverse to the last file in the path
-    // Directory curr = root;
-    // for (int i=1; i < pathway.length; i++) {
-    // // check if the next file in the path is a child of the current directory
-    // ArrayList<File> storedFiles = curr.getStoredFiles();
-    //
-    // }
-    // }
-    //
-    // }
   }
 
   private Directory rootDirectory = new Directory("root");
@@ -162,13 +73,12 @@ public class FileSystem {
       String newPathway[] = Arrays.copyOfRange(pathway, 1, pathway.length);
 
       // check if search directory is '..', then search if parent exists
-      // TODO I think it should be !curr.equals(rootDirectory)
       if (searchDir == ".." && !curr.equals(rootDirectory)) {
 
         returnFile = this.getFile(curr.getParent(), newPathway);
       } else {
 
-        // TODO raise error because no parent
+        // TODO raise no parent exception
       }
 
       while (i < storedFiles.size() && notFound) {
@@ -183,7 +93,8 @@ public class FileSystem {
       }
 
       if (notFound) {
-        // TODO raise error because the directory was not found
+
+        // TODO raise directory not found exception
       }
     }
     return returnFile;
@@ -208,6 +119,7 @@ public class FileSystem {
     }
 
     File returnFile;
+
     // searches the correct directory for the file in pathway
     if (relative) {
       returnFile = this.getFile(currentDirectory, pathway);
@@ -216,5 +128,4 @@ public class FileSystem {
     }
     return returnFile;
   }
-
 }
