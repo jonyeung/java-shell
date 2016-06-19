cd42f66763608f8ec551ba3ab2b7fc075e368050
xiezhang <xiezhang@UTORONTO.CA>
2016-06-19 20:45:49 +0000

    updated CommandException.java

--- Assignment2/src/driver/CommandException.java
+++ Assignment2/src/driver/CommandException.java
@@ -1,9 +1,14 @@
 package driver;
 
 // TODO Needs discussion. Refer to Monday, June 19th B07 lecture on exceptions
+
+/**
+ * The Command Exception class.
+ */
 public class CommandException extends Exception {
   
   public CommandException(String errorMessage) {
+
     super(errorMessage);
   }
 }
