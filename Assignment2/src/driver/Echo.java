672954d6452bb60a76528c74286d039a81a6f6c5
yeungj21 <yeungj21@UTORONTO.CA>
2016-06-25 06:38:36 +0000

    Added Echo.java and wrote first revision of its code

---
+++ Assignment2/src/driver/Echo.java
@@ -0,0 +1,62 @@
+package driver;
+
+import java.util.ArrayList;
+
+public class Echo {
+
+  /**
+   * Creates a new text file who's contents contain only string. If the text
+   * file already exists, it overwrites all its contents with the new string.
+   * 
+   * @ fileSys The file system being utilized @ string The text file contents @
+   * path The location of the text file @ chevrons Whether or not there are 2
+   * chevrons
+   * 
+   * @throws CommandException
+   */
+  public static void echoNew(FileSystem fileSys, String string, String path, 
+      Boolean chevrons) throws CommandException {
+    // get the name of the text file
+    String[] pathway = Interpreter.filepathToArray(path);
+    String fileName = pathway[pathway.length - 1];
+    
+    // determine if the path is relative or not
+    boolean relative = path.contains("/");
+    
+    Directory curr;
+    
+    if (relative) {
+      // check if the file exists in the current directory
+      curr = fileSys.getCurrentDirectory();
+      
+    } else {
+      // reach the parent directory of the text file
+      curr = fileSys.traversePath(path);
+    }
+    
+    if (curr.fileInDirectory(fileName)) {
+        // get the existing text file with the same name
+        ArrayList<File> storedFiles = curr.getStoredFiles();
+        for (File file : storedFiles) {
+          if (file.getName() == fileName) {
+            // set this file's contents to string
+           // try {
+           //   if (chevrons) {
+           //     ((TextFile) file).append(string);
+           //   } else {
+           //     ((TextFile) file).write(string);
+           //   }
+           // } catch {
+           //    TODO: THROW EXCEPTION: NOT A TEXTFILE
+           // }
+          }
+        }
+      } else {
+        // add a text file to this directory with contents string
+        TextFile newFile = new TextFile(fileName, string, curr);
+        curr.storeFile(newFile);
+      }  
+    
+    
+  }
+}
