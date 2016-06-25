26978dadba69b834710db0a1c53c71b10792d2ce
gillgu19 <gillgu19@UTORONTO.CA>
2016-06-25 00:41:36 +0000

    Suppressed warnings on commandexception

--- Assignment2/src/driver/CommandException.java
+++ Assignment2/src/driver/CommandException.java
@@ -5,6 +5,7 @@ package driver;
 /**
  * The Command Exception class.
  */
+@SuppressWarnings("serial")
 public class CommandException extends Exception {
 
   public CommandException(String errorMessage) {
