5179dcfa4f247158146ee62de0cbc3572e27302d
gillgu19 <gillgu19@UTORONTO.CA>
2016-06-26 03:38:46 +0000

    Made changes to printing the working directory in JShell.

--- Assignment2/src/driver/JShell.java
+++ Assignment2/src/driver/JShell.java
@@ -130,7 +130,7 @@ public class JShell {
     } else if (commandName.equals("ls")) {
       List.list(fileSystem, commandArgs);
     } else if (commandName.equals("pwd")) {
-      PrintWorkingDirectory.printWD(fileSystem);
+      System.out.println(PrintWorkingDirectory.printWD(fileSystem));
     } else if (commandName.equals("pushd")) {
       DirectoryStack.pushd(fileSystem, commandArgs[0]);
     } else if (commandName.equals("popd")) {
