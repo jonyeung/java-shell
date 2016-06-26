4321258fc2ce9082a250f77dd68b4d38ae2144be
gillgu19 <gillgu19@UTORONTO.CA>
2016-06-26 20:18:37 +0000

    Added Javadoc to CommandException.

--- Assignment2/src/driver/CommandException.java
+++ Assignment2/src/driver/CommandException.java
@@ -1,11 +1,16 @@
 package driver;
 
 /**
- * The Command Exception class.
+ * The command exception class for custom exceptions.
  */
 @SuppressWarnings("serial")
 public class CommandException extends Exception {
 
+  /**
+   * Custom exception that initializes the exception's error message
+   * 
+   * @param errorMessage The error message to be thrown to the user
+   */
   public CommandException(String errorMessage) {
 
     super(errorMessage);
