be65ab7b7c45e4e135106ec88fb558d274eee515
xiezhang <xiezhang@UTORONTO.CA>
2016-06-26 20:20:49 +0000

    added style

--- Assignment2/src/test/PrintWorkingDirectoryTest.java
+++ Assignment2/src/test/PrintWorkingDirectoryTest.java
@@ -14,26 +14,26 @@ public class PrintWorkingDirectoryTest {
   FileSystem fileSystem;
   String expectedOutput;
   String result;
-  
+
   @Before
   public void setup() {
     fileSystem = new FileSystem();
   }
-  
+
   @Test
   public void testPrintRootDirectory() {
     expectedOutput = "/";
     result = PrintWorkingDirectory.printWD(fileSystem);
     assertEquals(result, expectedOutput);
   }
-  
+
   @Test
   public void testPrintNewlyMadeDirectory() throws CommandException {
     expectedOutput = "/dir/";
     MakeDirectory.makeADirectory(fileSystem, "dir");
     ChangeDirectory.changeCurrentDirectory(fileSystem, "dir");
     result = PrintWorkingDirectory.printWD(fileSystem);
-    
+
     assertEquals(result, expectedOutput);
   }
 }
