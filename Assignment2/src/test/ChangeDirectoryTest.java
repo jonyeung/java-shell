c2dd14e280be2e3da7b91a15fe2903094df93aa6
gillgu19 <gillgu19@UTORONTO.CA>
2016-06-27 23:14:01 +0000

    Added unit tests for ChangeDirectory.java

---
+++ Assignment2/src/test/ChangeDirectoryTest.java
@@ -0,0 +1,77 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import driver.ChangeDirectory;
+import driver.CommandException;
+import driver.FileSystem;
+import driver.Directory;
+
+/**
+ * Class to test the ChangeDirectory method
+ */
+public class ChangeDirectoryTest {
+
+  FileSystem fileSystem;
+  String expectedResult;
+  String actualResult;
+  
+  @Before
+  public void setUp() throws Exception {
+    expectedResult = "";
+    actualResult = "";
+    fileSystem = new FileSystem();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    fileSystem = null;
+  }
+
+  /**
+   * Testing changing into directory before the root
+   * @throws CommandException
+   */
+  @Test
+  public void testChangingToInvalidRoot() throws CommandException {
+    try {
+    ChangeDirectory.changeCurrentDirectory(fileSystem, "..");
+    fail("You are currently at the root directory.");
+    } catch (CommandException e) {
+    }
+  }
+  
+  /**
+   * Testing changing into invalid directory
+   * @throws CommandException
+   */
+  @Test
+  public void testChangingToInvalidDirectory() throws CommandException {
+    try {
+    ChangeDirectory.changeCurrentDirectory(fileSystem, "invalidName");
+    fail("Directory invalidName does not exist.");
+    } catch (CommandException e) {
+    }
+  }
+  
+  /**
+   * Testing changing into valid directory
+   * @throws CommandException 
+   */
+  @Test
+  public void testChangingToValidDirectory() throws CommandException {
+    Directory validFile = new Directory("validDir");
+    fileSystem.getRootDirectory().storeFile(validFile);
+    ChangeDirectory.changeCurrentDirectory(fileSystem, "validDir");
+    expectedResult = "/validDir/";
+    actualResult = "/" + fileSystem.getCurrentDirectory().getName() + "/";
+    assertEquals(actualResult, expectedResult);
+  }
+  
+  
+
+}
