7bcb0c1f2344a5d1d23c144cd90ebe5e9ce63040
xiezhang <xiezhang@UTORONTO.CA>
2016-06-28 00:30:20 +0000

    updated DirectoryTest

--- Assignment2/src/test/DirectoryTest.java
+++ Assignment2/src/test/DirectoryTest.java
@@ -17,17 +17,22 @@ public class DirectoryTest {
 
   @Before
   public void setUp() {
+
     parent = new Directory("parent");
   }
 
   @After
   public void cleanUp() {
+
     parent = null;
   }
 
+  /**
+   * test that store file puts the given file into the directory's list
+   */
   @Test
   public void testStoreFile() {
-    // testing that store file puts the given file into the directory's list
+
     File file1 = new File("file1");
     File file2 = new File("file2");
     parent.storeFile(file1);
@@ -39,10 +44,13 @@ public class DirectoryTest {
     assertEquals(output, expected);
   }
 
+  /**
+   * test that store file stores the files in alphabetical order, regardless of
+   * when a file was stored
+   */
   @Test
   public void testStoreFileAlphabetical() {
-    // testing that store file stores the files in alphabetical order,
-    // regardless of when a file was stored
+
     File file1 = new File("c");
     File file2 = new File("b");
     File file3 = new File("a");
@@ -59,14 +67,16 @@ public class DirectoryTest {
 
   @Test
   public void testFileInDirectoryFileExists() {
+
     // testing file in directory in the event where the file is in the directory
     File file1 = new File("file1");
     parent.storeFile(file1);
     assertTrue(parent.fileInDirectory("file1"));
   }
-  
+
   @Test
   public void testFileInDirectoryFileDoesNotExists() {
+
     // testing file in directory in the event where the file is not in
     // the directory
     assertFalse(parent.fileInDirectory("file1"));
