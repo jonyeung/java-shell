8e0f77a1115b4a541b2906593a74428273283c62
yeungj21 <yeungj21@UTORONTO.CA>
2016-06-28 00:02:07 +0000

    Added javadocs to FileTest

--- Assignment2/src/test/FileTest.java
+++ Assignment2/src/test/FileTest.java
@@ -9,27 +9,34 @@ import driver.File;
 
 public class FileTest {
 
+  /**
+   * Test that a file without a parent returns nothing when getParent is called
+   */
   @Test
   public void testFileWithNoParent() {
-    // test that a file without a parent returns nothing when getParent is
-    // called
     File file = new File("my file");
     assertEquals(file.getParent(), null);
   }
 
+  /**
+   * Test that a file with a parent returns that parent file when getParent is
+   * called
+   */
   @Test
   public void testFileWithParent() {
-    // test that a file with a parent returns that parent file when getParent
-    // is called
+
     Directory parent = new Directory("parent directory");
     File file = new File("my file", parent);
     assertEquals(file.getParent(), parent);
   }
 
+  /**
+   * Test that a file constructed with a given name returns that name when
+   * getName is called
+   */
   @Test
   public void testFileHasCorrectName() {
-    // test that a file constructed with a given name returns that name when
-    // getName is called
+
     File file = new File("my file");
     assertEquals(file.getName(), "my file");
   }
