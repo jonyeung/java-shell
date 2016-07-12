732082f46dada9a9abc107b07fd199c4b32e33dc
yeungj21 <yeungj21@UTORONTO.CA>
2016-07-12 01:42:18 +0000

    Fixed Move.java so that a copy command fully copies a file

--- Assignment2/src/driver/Move.java
+++ Assignment2/src/driver/Move.java
@@ -10,7 +10,7 @@ public class Move {
    * @param oldPath The location of the file to be moved
    * @param newPath The destination of the file that is being moved
    * @param deleteOriginal True if the item is being moved and not copied
-   * @throws CommandException
+   * @throws CommandException if the newPath does not specify a directory
    */
   public static void moveItem(FileSystem fileSys, String oldPath, 
       String newPath, boolean deleteOriginal) throws CommandException {
@@ -20,6 +20,13 @@ public class Move {
     if (deleteOriginal) {
       // remove this item from its parent directory
       item.getParent().getStoredFiles().remove(item);
+    } else {
+      // make a copy of the item
+      if (item instanceof Directory) {
+        item = Move.copyDirectory((Directory)item);
+      } else {
+        item = Move.copyTextFile((TextFile)item);
+      }
     }
       
     // get the directory specified at newPath
@@ -33,4 +40,36 @@ public class Move {
 
   }
 
+  /**
+   * Makes a copy of the given directory
+   * 
+   * @param Directory The directory being copied
+   * @return Directory A copy of the directory
+   */
+  private static Directory copyDirectory(Directory file) {
+    // recreate the directory with the same name and parent file
+    Directory copy = new Directory(file.getName(), file.getParent());
+    // recreate all stored files
+    for (File item : ((Directory) file).getStoredFiles()) {
+      if (item instanceof Directory) {
+        copy.storeFile(Move.copyDirectory((Directory)item));
+      } else {
+        copy.storeFile(Move.copyTextFile((TextFile)item));
+      }
+    }
+    return copy;
+  }
+  
+  /**
+   * Makes a copy of the given text file
+   * 
+   * @param TextFile The text file being copied
+   * @return TextFile A copy of the text file
+   */
+  private static TextFile copyTextFile(TextFile file) {
+    // recreate the text file with the same name, contents, and parent
+    TextFile copy = new TextFile(file.getName(), 
+          ((TextFile) file).getFileContents(), file.getParent());
+    return copy;
+  }
 }
