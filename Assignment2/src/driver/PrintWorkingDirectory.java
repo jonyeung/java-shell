e7c55f0b4514956fa75b6ca29e49ab0754c8c66d
chanja49 <chanja49@UTORONTO.CA>
2016-07-16 02:52:05 +0000

    split up pwd method into two

--- Assignment2/src/driver/PrintWorkingDirectory.java
+++ Assignment2/src/driver/PrintWorkingDirectory.java
@@ -7,27 +7,34 @@ package driver;
 public class PrintWorkingDirectory {
 
   /**
-   * Finds and returns the current working directory
+   * Finds and returns the absolute path of curr.
    * 
    * @param fileSystem The file system with all the files and directories
+   * @param curr The current file that we want the absolute path of
    * @return String The pathway of the current working directory
    */
-  public static String printWD(FileSystem fileSystem) {
-
-    // Get the current directory
-    Directory file = fileSystem.getCurrentDirectory();
+  public static String getAbsoluteFilepath(FileSystem fileSystem, File curr) {
 
     // Put the pathway string together starting with the current file
     String pathway = "/";
 
     // Add to the pathway all the parents of the current file
-    File curr = file;
-
     while (curr.getParent() != null) {
       pathway = "/" + curr.getName() + pathway;
       curr = curr.getParent();
     }
 
     return pathway;
+  }
+  
+  /**
+   * Finds and returns the current working directory
+   * 
+   * @param fileSystem The file system with all the files and directories
+   * @return String The pathway of the current working directory
+   */
+  public static String printWD(FileSystem fileSystem) {
+    
+    return getAbsoluteFilepath(fileSystem, fileSystem.getCurrentDirectory());
   }
 }
